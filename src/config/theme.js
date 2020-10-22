16bb792a6255ef9af9645207c0fad2d3f70ecedd
irwantowibowotech <irwantowibowotech@gmail.com>
2020-10-22 09:11:24 +0700

    styling form register

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,9 +1,11 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import red from "@material-ui/core/colors/red";
+import green from "@material-ui/core/colors/green";
+import lightBlue from "@material-ui/core/colors/lightBlue";
 
 const theme = createMuiTheme({
   palette: {
-    primary: red,
+    primary: green,
+    secondary: lightBlue,
   },
 });
 
